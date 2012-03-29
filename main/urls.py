0368a714db6306d2e9c64ad1243eee012a4d2940
Giles Thomas <giles.thomas@resolversystems.com>
2012-03-29 22:40:45 +0000

    Added list of jobs to front page.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
-urlpatterns = patterns('',
+from webgljobs.main.models import Job
+
+urlpatterns = patterns("",
     url(
-        r'^$', 
-        direct_to_template, 
-        {'template': 'main_page.html'},
-        name='main_page'
+        r"^$",
+        direct_to_template,
+        {
+            "template": "main_page.html",
+            "extra_context": {
+                "jobs": Job.objects.all
+            }
+        },
+        name="main_page"
     ),
 )
