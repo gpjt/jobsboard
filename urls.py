d6b023d3fb027c4224ad4b9ca4144bfa523aaf7c
Giles Thomas <giles.thomas@resolversystems.com>
2012-03-29 18:30:05 +0000

    Enabled Django admin.

--- urls.py
+++ urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
@@ -15,8 +14,7 @@ urlpatterns = patterns('',
         name='main_page'
     ),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
