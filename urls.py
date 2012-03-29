852e074a780848d8afc19cf10fc691d94bbd00d7
Giles Thomas <giles.thomas@resolversystems.com>
2012-03-29 22:24:48 +0000

    Moved main page URL config to main app.

--- urls.py
+++ urls.py
@@ -1,19 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(
-        r'^$', 
-        direct_to_template, 
-        {'template': 'main_page.html'},
-        name='main_page'
-    ),
-
+    url(r'^', include('webgljobs.main.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
