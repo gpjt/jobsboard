eaac16fbc557770e662044ee8c57c146fbb75739
Giles Thomas <giles.thomas@resolversystems.com>
2012-03-29 18:12:51 +0000

    Placeholder front page, added bootstrap CSS.

--- settings.py
+++ settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'webgljobs.main',
 )
 
 # A sample logging configuration. The only tangible logging
--- urls.py
+++ urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webgljobs.views.home', name='home'),
-    # url(r'^webgljobs/', include('webgljobs.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+urlpatterns = patterns('',
+    url(
+        r'^$', 
+        direct_to_template, 
+        {'template': 'main_page.html'},
+        name='main_page'
+    ),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
