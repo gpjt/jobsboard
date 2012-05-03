f9580efd769459861fc78382e51d5be09b907dcd
Giles Thomas <giles.thomas@resolversystems.com>
2012-05-03 13:51:34 +0000

    Added thanks page for post new job.

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.core.urlresolvers import reverse
 from django.views.generic.simple import direct_to_template
 from django.views.generic.create_update import create_object
 
@@ -31,7 +32,16 @@ urlpatterns = patterns("",
         {
             "form_class": JobForm,
             "template_name": "create_new_job.html",
+            "post_save_redirect": "/add/thanks",
         },
         name="create_new_job"
+    ),
+    url(
+        r"^add/thanks$",
+        direct_to_template,
+        {
+            "template": "create_new_job_thanks.html",
+        },
+        name="create_new_job_thanks"
     ),
 )
