642104da62d96dfb3df541e922b2e42076977573
Giles Thomas <giles.thomas@resolversystems.com>
2012-09-22 20:47:49 +0000

    Get the jobs in the approve/disapprove views.

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from webgljobs.main.models import Job
 
 
 def approve(request, object_id):
-    return HttpResponse("Approve %s" % (object_id,))
+    job = get_object_or_404(Job, pk=object_id)
+    return HttpResponse("Approve %s" % (job.id,))
 
 
 def disapprove(request, object_id):
-    return HttpResponse("Disapprove %s" % (object_id,))
+    job = get_object_or_404(Job, pk=object_id)
+    return HttpResponse("Disapprove %s" % (job.id,))
