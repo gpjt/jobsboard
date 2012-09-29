1a896d189ef83bc2096589d2e2a33f8b33a5222a
Giles Thomas <giles.thomas@resolversystems.com>
2012-09-29 10:54:14 +0000

    Construct (but don't send) a tweet when approving.

--- main/forms.py
+++ main/forms.py
@@ -7,3 +7,11 @@ class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         exclude = ('approved', 'filled',)
+
+
+class TweetForm(forms.Form):
+    # 140-characters for tweet
+    # - 17 for "New #WebGL #Job: "
+    # - 1 char for space after summary.
+    # - 20 for t.co URL
+    summary = forms.CharField(max_length=102, required=False)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.contrib.sites.models import Site
 
+from webgljobs.main.forms import TweetForm
 from webgljobs.main.models import Job
 
 
@@ -9,13 +11,19 @@ from webgljobs.main.models import Job
 @staff_member_required
 def approve(request, object_id):
     job = get_object_or_404(Job, pk=object_id)
-    if request.method == "GET":
-        return render(request, "job_approve_confirm.html", { "job": job })
-    if request.POST["confirmed"] != "yes":
-        return HttpResponse("Erroneous form submission, should never happen...")
-    job.approved = True
-    job.save()
-    return render(request, "job_approve_done.html", { "job": job })
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            job.approved = True
+            job.save()
+            site = Site.objects.all()[0]
+            job_url = "http://%s%s" % (site.domain, job.get_absolute_url())
+            tweet = "New #WebGL #Job: %s %s" % (form.cleaned_data["summary"], job_url)
+            return render(request, "job_approve_done.html", { "job": job, "tweet": tweet })
+    else:
+        form = TweetForm()
+
+    return render(request, "job_approve_confirm.html", { "job": job, "form": form })
 
 
 @staff_member_required
